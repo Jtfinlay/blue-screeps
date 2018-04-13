6127902a90a5f54e93de8d2c1e398adccd5d67fd
James Finlay <jtfinlay@ualberta.ca>
2018-04-12 21:17:25 -0700

    have creeps move again

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import { ErrorMapper } from './utils/ErrorMapper';
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-    console.log(`Current game tick is ${Game.time}`);
 
     // Automatically delete memory of missing creeps
     for (const name in Memory.creeps) {
         if (!(name in Game.creeps)) {
-        delete Memory.creeps[name];
+            delete Memory.creeps[name];
         }
     }
 
@@ -18,5 +17,10 @@ export const loop = ErrorMapper.wrapLoop(() => {
         if (spawn.energy >= 200) {
             spawn.createCreep( [ WORK, CARRY, MOVE], 'Creep'+Game.time);
         }
+    }
+
+    for (const name in Game.creeps){
+        const creep = new CreepModel(Game.creeps[name]);
+        creep.run();
     }
 });
